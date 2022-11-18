aac59499199d41b8d0d520cfa170854ac9f0d379
johnsonsudre <johnsonsudre@gmail.com>
2022-11-18 06:41:24 -0300

    Initial project

---
+++ App.js
@@ -0,0 +1,31 @@
+import "./style.css";
+import * as THREE from "three";
+
+function getAspectRatio() {
+  return window.innerWidth / window.innerHeight;
+}
+
+class App {
+  constructor() {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+
+    this.camera = new THREE.PerspectiveCamera(60, getAspectRatio(), 0.02, 10);
+
+    window.addEventListener("resize", this.resize.bind(this));
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    container.appendChild(this.renderer.domElement);
+  }
+
+  resize() {
+    this.camera.aspect = getAspectRatio();
+    this.camera.updateProjectionMatrix(); // so as not to distort the render
+    console.log(window.innerWidth, window.innerHeight)
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+}
+
+export { App };
