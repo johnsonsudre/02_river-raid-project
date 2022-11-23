474397592de91a0d8e47c93124f1cac61cf7eff9
johnsonsudre <johnsonsudre@gmail.com>
2022-11-23 07:15:24 -0300

    set Obstacles constructor

--- core/Obstacles.js
+++ core/Obstacles.js
@@ -1,11 +1,22 @@
-export class Obstacles{
-    constructor(game){
-        this.ready = true;
-    }
-    reset(){
-        
-    }
-    update(playerPos){
-        // console.log(pos)
-    }
+import { Vector3, Group } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+export class Obstacles {
+  constructor(game) {
+    this.assetsPath = game.assetsPath;
+    this.loadingBar = game.loadingBar;
+    this.game = game;
+    this.scene = game.scene;
+    this.loadPlant();
+    this.loadObstacle();
+    this.load;
+    this.ready = true;
+    this.tmpPos = new Vector3();
+  }
+  loadPlant() {}
+  loadObstacle() {}
+  reset() {}
+  update(playerPos) {
+    // console.log(pos)
+  }
 }
