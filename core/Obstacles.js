7395409a511dc8a4e2e79942bae1c13d97027ac0
johnsonsudre <johnsonsudre@gmail.com>
2022-11-23 14:14:00 -0300

    loading obstacles

--- core/Obstacles.js
+++ core/Obstacles.js
@@ -7,16 +7,50 @@ export class Obstacles {
     this.loadingBar = game.loadingBar;
     this.game = game;
     this.scene = game.scene;
-    this.loadPlant();
-    this.loadObstacle();
+    this.loadPlant.bind(this);
+    this.loadBomb.bind(this);
     this.load;
     this.ready = true;
     this.tmpPos = new Vector3();
   }
-  loadPlant() {}
-  loadObstacle() {}
+
+  loadPlant() {
+    const loader = new GLTFLoader().setPath(`${this.assetsPath}/obstacles/`);
+    this.ready = false;
+    loader.load(
+      "plant.gltf",
+      (gltf) => {
+        this.plant = gltf.scene.children[0];
+        this.plant.name = "plant";
+        if (this.bomb !== undefined) this.initialize();
+      },
+      (xhr) => {
+        this.loadingBar("plant", xhr.loaded, xhr.total);
+      }
+    );
+  }
+
+  loadBomb() {
+    const loader = new GLTFLoader().setPath(`${this.assetsPath}/obstacles/`);
+    this.ready = false;
+    loader.load(
+      "bomb.gltf",
+      (gltf) => {
+        this.plant = gltf.scene.children[0];
+        this.plant.name = "bomb";
+        if (this.plant !== undefined) this.initialize();
+      },
+      (xhr) => {
+        this.loadingBar("bomb", xhr.loaded, xhr.total);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+  initialize() {}
   reset() {}
   update(playerPos) {
-    // console.log(pos)
+    // console.log(playerPos)
   }
 }
